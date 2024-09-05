f394b0d93778267f3935fa56622b7014defa6771
Mostafa Kamel <mostafamt60@yahoo.com>
2024-09-05 08:07:44 +0300

    update vite

--- cardatabase-frontend/vite.config.ts
+++ cardatabase-frontend/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import glob from "glob";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +9,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+  },
+  base: "",
+  root: "src",
+  build: {
+    outDir: "../dist",
   },
 });
